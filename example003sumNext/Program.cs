51800a7c7905281851d17552dd3394a8f47f3e39
ruslan <rus.pro94@bk.ru>
2022-09-16 10:45:09 +0500

    .\example003sumNext\

---
+++ example003sumNext/Program.cs
@@ -0,0 +1,9 @@
+﻿int namberA = new Random().Next(1,10);
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
+int result = namberA + namberB;
+Console.WriteLine(result);
+
+
+
